e5abc54c232738ce048991f76f323441a71ec34c
watcharaphon_p <watcharapon590@gmail.com>
2026-02-10 00:49:22 +0700

    connect backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'YOUR_BACKEND_URL/api/todos' // We will update this after deploying backend
+    apiUrl: 'https://todo-backend-zbpt.onrender.com/api/todos'
 };
